218b051bcb6f06d42260a951488cae0fb1776261
Jesse Anderson <sockeyes51@gmail.com>
2016-12-03 00:32:21 -0500

    playing with deploying docker images

--- LAMPDeployCookbook/recipes/docker_install.rb
+++ LAMPDeployCookbook/recipes/docker_install.rb
@@ -4,12 +4,11 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
+cookbook_file 'Dockerfile' do
+  path 'tmp/Dockerfile'
+  source 'default/Dockerfile'
+end
 
 docker_service 'default' do
   action [:create, :start]
-end
-
-docker_image 'hello-world' do
-  tag 'latest'
-  action :pull
 end
