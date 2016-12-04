6e4d6e88d1a8678bb5da4fc3f5211615795e4e7f
Sean Luce <lucesean@gmail.com>
2016-12-04 15:20:20 -0500

    Added stuff to docker_install.rb

--- LAMPDeployCookbook/recipes/docker_install.rb
+++ LAMPDeployCookbook/recipes/docker_install.rb
@@ -16,3 +16,34 @@ end
 docker_image 'httpd' do
   action :pull_if_missing
 end
+ 
+# Pull latest image
+docker_image 'nginx' do
+  tag 'latest'
+  action :pull
+  notifies :redeploy, 'docker_container[my_nginx]'
+end
+
+# Run container exposing ports
+docker_container 'my_nginx' do
+  repo 'nginx'
+  tag 'latest'
+  port '8080:80'
+  volumes [ '/home/seanluce/nginx:/usr/share/nginx/html' ]
+end
+
+# Pull latest image
+docker_image 'httpd' do
+  tag 'latest'
+  action :pull
+  notifies :redeploy, 'docker_container[my_httpd]'
+end
+
+# Run container exposing ports
+docker_container 'my_httpd' do
+  repo 'httpd'
+  tag 'latest'
+  port '8090:80'
+  volumes [ '/home/seanluce/httpd:/usr/local/apache2/htdocs/' ]
+end
+
