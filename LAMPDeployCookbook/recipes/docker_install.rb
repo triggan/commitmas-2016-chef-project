43635f7fdb52c4695ba963fbf5efefb46248f38e
Jesse Anderson <sockeyes51@gmail.com>
2016-12-03 22:16:02 -0500

    playing with docker commands

--- LAMPDeployCookbook/recipes/docker_install.rb
+++ LAMPDeployCookbook/recipes/docker_install.rb
@@ -4,10 +4,6 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-cookbook_file 'Dockerfile' do
-  path 'tmp/Dockerfile'
-  source 'default/Dockerfile'
-end
 
 docker_service 'default' do
   action [:create, :start]
